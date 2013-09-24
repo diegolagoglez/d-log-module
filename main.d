972f94ec213b4ec5f86a1ad869a211c76af5e3c0
Diego Lago <diego.lago.gonzalez@gmail.com>
2013-09-24 17:06:04 +0200

    Mejora del ejemplo.

--- main.d
+++ main.d
@@ -9,13 +9,22 @@ main() {
 	log.addLogWriter(new ConsoleLogWriter);
 	
 	log.emergency("Esto es una emergencia.");
+	log.emergency("Esto es una emergencia con nombre: %s", "Juanito");
+	log.em("Emergencia con método abreviado.");
 	log.alert("Esto es una alerta.");
+	log.a("Alerta con método abreviado.");
 	log.critical("Esto es un mensaje crítico.");
+	log.c("Error crítico con método abreviado.");
 	log.error("Esto es un error.");
+	log.e("Error con método abreviado.");
 	log.notice("Esto es algo que hay que tener en cuenta.");
+	log.n("Notice con método abreviado.");
 	log.info("Esto es información.");
+	log.i("Información con método abreviado.");
 	log.dbg("Esto es una línea de depuración.");
+	log.d("Depuración con método abreviado.");
 	log.trace("Esto es una línea de depuración pero a más bajo nivel.");
+	log.t("Trace con método abreviado.");
 
 	return 0;
 }
